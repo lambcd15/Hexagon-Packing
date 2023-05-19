141536dc0169112090193a734f47b7f79a59a02f
lambcd15 <lambcd15@gmail.com>
2023-05-19 14:59:23 +0800

    first

---
+++ Hexagon_MK1.py
@@ -0,0 +1,7 @@
+# Python file with commands for github
+
+# To setup new repo
+# Make new folder then navigate to it in Bash
+# type "git init" once inside folder
+# Then "git add" (ensure there are sopme files in the folder)
+
